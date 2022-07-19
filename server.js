06f0b67a1a2d42563f3925978555668ede9a1bb4
Zach Burkhart <zachburkhart216@gmail.com>
2022-07-19 15:36:28 -0400

    changed port to 3001

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const sequelize = require("./config/connection");
